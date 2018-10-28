0d130fd3138b530534a532fcc2f4185ec8d598f5
Mauser <filip1209348756@gmail.com>
2018-10-28 12:25:46 +0100

    Added working "Hello World!" project configured for Cmake. Look into "build" folder for compiled Program.exe test.

---
+++ Projekt_1/build/program/CMakeFiles/Program.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "A:/Kod_projekty/Krypto_Repo/Krypto2018/Projekt_1/program/source/main.cpp" "A:/Kod_projekty/Krypto_Repo/Krypto2018/Projekt_1/build/program/CMakeFiles/Program.dir/source/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../program/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Projekt_1/build/program/CMakeFiles/Program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program.dir/source/main.cpp.obj"
+  "Program.pdb"
+  "Program.exe"
+  "Program.exe.manifest"
+  "libProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
