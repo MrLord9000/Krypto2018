2071c0a7ce52851d689c008f93454daea378d016
Mauser <filip1209348756@gmail.com>
2018-11-03 20:46:57 +0100

    Deleted unnecessary build folder and all its contents. Please do not commit changes from the build folder. Gitignore should do this automatically but you know how it is...

--- Projekt_1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: A:/Kod_projekty/Krypto_Repo/Krypto2018/Projekt_1
-# Build directory: A:/Kod_projekty/Krypto_Repo/Krypto2018/Projekt_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
